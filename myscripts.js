97b4370503318d8d1de30d531acaa386110e2b64
Chris Durand <44622682+CDurandLS@users.noreply.github.com>
2020-06-26 19:06:13 -0500

    Cleaned up, removed increasing speed

--- myscripts.js
+++ myscripts.js
@@ -14,11 +14,11 @@ let Point = function(sx, sy){
   this.y = sy;
 }
 let Snake = function(startx, starty) {
-  this.speed = (framerate/20);
+  this.speed = (360/framerate);
   this.bodyLength = 1;
   this.body = [new BodyPart(startx, starty, 0, 0)];
   this.dead = false;
-  this.turningSpeed = ((Math.PI*2)/30)/(framerate/20);
+  this.turningSpeed = ((Math.PI*2)/30)/(framerate/40);
 };
 Snake.prototype.move = function() {
   this.body[0].moveAlone(this.speed);
@@ -26,18 +26,6 @@ Snake.prototype.move = function() {
     this.body[i].moveToLeader();
   }
   this.checkIfDead();
-}
-Snake.prototype.getPolyPoints = function() {
-  let points = [];
-  let rightPoints = [];
-  for (let i=0; i < this.body.length;i++){
-    points.push(this.body[i].leftSidePoint());
-  }
-  for (let i=0; i < this.body.length;i++){
-    let j = this.body.length - 1 - i;
-    points.push(this.body[j].rightSidePoint());
-  }
-  return points;
 }
 Snake.prototype.checkIfDead = function() {
   let head = this.body[0];
@@ -62,17 +50,13 @@ Snake.prototype.draw = function() {
     let points = this.body[i].pointsToLeader();
     ctx.beginPath();
     ctx.moveTo(points[0].x, points[0].y);
-    ctx.lineTo(points[1].x, points[1].y);
-    ctx.lineTo(points[2].x, points[2].y);
-    ctx.lineTo(points[3].x, points[3].y);
+    for(let j = 1; j < 4; j++){
+      ctx.lineTo(points[j].x, points[j].y);
+    }
     ctx.closePath();
     ctx.fill();
   }
 }
-// Snake.prototype.draw = function() {
-//   console.log("drawing the snake");
-//   this.body.forEach(part => part.draw());
-// }
 Snake.prototype.turn = function(direction) {
   if(direction == 'left'){
     this.body[0].heading -= this.turningSpeed;
@@ -83,8 +67,6 @@ Snake.prototype.turn = function(direction) {
 }
 Snake.prototype.eat = function() {
   let tail = this.body[this.body.length-1];
-  this.turningSpeed *= 1.05;
-  this.speed *= 1.05;
   this.body.push(
     new BodyPart(
       tail.behindx(),
@@ -100,8 +82,6 @@ let BodyPart = function(bx, by, direction, leader) {
   this.y = by;
   this.heading = direction;
   this.leader = leader;
-  this.towardsx = leader.x;
-  this.towardsy = leader.y;
   this.color = "#77CC77";
 };
 BodyPart.prototype.isCollidingWith = function(obj) {
@@ -118,6 +98,12 @@ BodyPart.prototype.moveAlone = function(amount) {
   this.y += amount * Math.sin(this.heading);
 
 }
+BodyPart.prototype.behindx = function() {
+  return this.x - 2 * radius * Math.cos(this.heading)
+}
+BodyPart.prototype.behindy = function() {
+  return this.y - 2 * radius * Math.sin(this.heading)
+}
 BodyPart.prototype.moveToLeader = function() {
   let dx = (this.x - this.leader.x);
   let dy = (this.y - this.leader.y);
@@ -129,41 +115,18 @@ BodyPart.prototype.moveToLeader = function() {
 BodyPart.prototype.draw = function() {
   drawCircle(this.x, this.y, radius, this.color);
 }
-BodyPart.prototype.behindx = function() {
-  return this.x - 2 * radius * Math.cos(this.heading)
-}
-BodyPart.prototype.behindy = function() {
-  return this.y - 2 * radius * Math.sin(this.heading)
-}
 BodyPart.prototype.pointsToLeader = function() {
   let points = [new Point(0,0),new Point(0,0),new Point(0,0),new Point(0,0)];
-  points[0].x = this.x + Math.cos(this.heading + Math.PI/2) * radius;
-  points[0].y = this.y + Math.sin(this.heading + Math.PI/2) * radius;
+  points[0].x = this.x        + Math.cos(this.heading + Math.PI/2) * radius;
+  points[0].y = this.y        + Math.sin(this.heading + Math.PI/2) * radius;
   points[1].x = this.leader.x + Math.cos(this.heading + Math.PI/2) * radius;
   points[1].y = this.leader.y + Math.sin(this.heading + Math.PI/2) * radius;
   points[2].x = this.leader.x + Math.cos(this.heading - Math.PI/2) * radius;
   points[2].y = this.leader.y + Math.sin(this.heading - Math.PI/2) * radius;
-  points[3].x = this.x + Math.cos(this.heading - Math.PI/2) * radius;
-  points[3].y = this.y + Math.sin(this.heading - Math.PI/2) * radius;
+  points[3].x = this.x        + Math.cos(this.heading - Math.PI/2) * radius;
+  points[3].y = this.y        + Math.sin(this.heading - Math.PI/2) * radius;
   return points;
 }
-BodyPart.prototype.leftSidePoint = function() {
-  let point = {
-    x: 0,
-    y: 0
-  }
-  return point;
-}
-BodyPart.prototype.rightSidePoint = function() {
-  let point = {
-    x: 0,
-    y: 0
-  }
-  point.x = this.x + Math.cos(this.heading - Math.PI/2) * radius;
-  point.y = this.y + Math.sin(this.heading - Math.PI/2) * radius;
-  return point;
-}
-
 
 let Food = function() {
   this.x = Math.random() * 700 + 50;
@@ -224,7 +187,6 @@ function writeInstructions() {
   ctx.fillRect(797, 0, 800, 800);
   ctx.fillRect(0, 797, 800, 800);
   ctx.fillRect(0, 0, 3, 800);
-
   ctx.fillText("Use the left and right arrow keys to move the snake.", 400, 50);
 }
 setUpGame();
@@ -234,12 +196,12 @@ function gameUpdate() {
   snake.move();
   snake.checkIfDead();
   snake.checkIfAte();
+  draw();
   if (snake.dead) {
     gameScore = snake.body.length - 1;
     alert(`You died! your score is ${gameScore}`);
     setUpGame();
   }
-  draw();
 }
 setInterval(gameUpdate, 1000/framerate);
 // setInterval(snake.eat, 1000);
